8f24ed355b06d41bb8c8e512e9a01fc07e2d06e1
Darius Jankauskas <d5926j@gmail.com>
2019-09-18 01:37:24 -0400

    Added pie and bar charts.

--- lib/extensions.ts
+++ lib/extensions.ts
@@ -1,6 +1,8 @@
 import * as React from 'react';
 
 import { LineChartExtension } from './LineChartExtension';
+import { BarChartExtension } from './BarChartExtension';
+import { PieChartExtension } from './PieChartExtension'
 
 //The props are parsed from the json-encoded props field in an article-extension.
 export interface IExtensionProps {
@@ -8,5 +10,7 @@ export interface IExtensionProps {
 }
 
 export const extensions: Array<[string, React.ComponentType<IExtensionProps>]> = [
-    ["LineChartExtension", LineChartExtension]
+    ["LineChartExtension", LineChartExtension],
+    ["BarChartExtension", BarChartExtension],
+    ["PieChartExtension", PieChartExtension]
 ];
