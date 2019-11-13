0889cb7aadb647d348fd8f487159bed3bee577ae
Darius Jankauskas <d5926j@gmail.com>
2019-11-13 15:02:58 -0500

    Add MediaExtension.

--- lib/extensions.ts
+++ lib/extensions.ts
@@ -1,16 +1,20 @@
 import * as React from 'react';
 
-import { LineChartExtension } from './LineChartExtension';
-import { BarChartExtension } from './BarChartExtension';
-import { PieChartExtension } from './PieChartExtension'
+import { LineChartExtension } from './charts/LineChartExtension';
+import { BarChartExtension } from './charts/BarChartExtension';
+import { PieChartExtension } from './charts/PieChartExtension';
+
+import { MediaExtension } from './media/MediaExtension';
 
 //The props are parsed from the json-encoded props field in an article-extension.
 export interface IExtensionProps {
     props: any,
+    media?: any[]
 }
 
-export const extensions: Array<[string, React.ComponentType<IExtensionProps>]> = [
+export const extensions: Map<string, React.ComponentType<IExtensionProps>> = new Map([
     ["LineChartExtension", LineChartExtension],
     ["BarChartExtension", BarChartExtension],
-    ["PieChartExtension", PieChartExtension]
-];
+    ["PieChartExtension", PieChartExtension],
+    ["MediaExtension", MediaExtension],
+]);
